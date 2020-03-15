dbad2ffb157f16a8d80a2dab44e2811d2125f80f
Oskar Schlösinger <oskar.schloesinger@ibm.com>
2020-03-15 21:26:34 +0100

    add carbon styles

--- covid-19-website/gatsby-config.js
+++ covid-19-website/gatsby-config.js
@@ -1,8 +1,10 @@
+const path = require("path")
+
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `COVID - 19 Overview`,
+    description: `An overview of covid 19 overview.`,
+    author: `@norraell`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -27,6 +29,8 @@ module.exports = {
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
+    "gatsby-plugin-sass",
+
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
--- covid-19-website/src/pages/index.js
+++ covid-19-website/src/pages/index.js
@@ -1,13 +1,15 @@
 import React from "react"
 import { Link } from "gatsby"
 
+import "../styles/index.scss"
+
 import Layout from "../components/layout"
 import Image from "../components/image"
 import SEO from "../components/seo"
 
 const IndexPage = () => (
   <Layout>
-    <SEO title="Home" />
+    <SEO title="Home" lang="DE" />
     <h1>Hi people</h1>
     <p>Welcome to your new Gatsby site.</p>
     <p>Now go build something great.</p>
@@ -15,6 +17,7 @@ const IndexPage = () => (
       <Image />
     </div>
     <Link to="/page-2/">Go to page 2</Link>
+    <Link to="/overview">Go to overview</Link>
   </Layout>
 )
 
