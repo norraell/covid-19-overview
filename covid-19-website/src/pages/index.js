f7f3ee21d5575ba9c6a7e28fec8155d3b5d760f1
Oskar Schlösinger <oskar.schloesinger@ibm.com>
2020-03-15 21:59:32 +0100

    changed text

--- covid-19-website/src/pages/index.js
+++ covid-19-website/src/pages/index.js
@@ -10,7 +10,7 @@ import SEO from "../components/seo"
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" lang="DE" />
-    <h1>Hi people</h1>
+    <h1>COVID 19 overview</h1>
     <p>Welcome to your new Gatsby site.</p>
     <p>Now go build something great.</p>
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
